64ee0a761b14eee8146a9028df4c3d3e025bf658
sky971126 <sky971126@163.com>
2019-04-24 20:56:48 -0400

    Update test_eval.py

--- test_eval.py
+++ test_eval.py
@@ -2,4 +2,4 @@ from nlgeval import NLGEval
 nlgeval = NLGEval()  # loads the models
 references = [["I am superman","Superman is me"], ["dog", "cat"]]
 hypothesis = ["superman is me", "dog"]
-metrics_dict = nlgeval.compute_metrics(references, hypothesis)
+print(nlgeval.compute_metrics(references, hypothesis))
